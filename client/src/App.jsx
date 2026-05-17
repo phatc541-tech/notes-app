e3b0a20ca4bc8cff94966f574b873c6b9c6e49b1
Châu Tuấn Phát <chautuanphat@MacBook-Pro-cua-Chau.local>
2026-05-17 15:21:59 +0700

    add profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,65 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+
+  BrowserRouter,
+  Routes,
+  Route
+
+} from "react-router-dom";
 
 import Login from "./pages/Login";
+
 import Register from "./pages/Register";
+
 import Home from "./pages/Home";
 
+import Profile from "./pages/Profile";
+
 function App() {
+
   return (
+
     <BrowserRouter>
+
       <Routes>
 
         <Route
+
           path="/"
+
           element={<Home />}
+
         />
 
         <Route
+
           path="/login"
+
           element={<Login />}
+
         />
 
         <Route
+
           path="/register"
+
           element={<Register />}
+
+        />
+
+        <Route
+
+          path="/profile"
+
+          element={<Profile />}
+
         />
 
       </Routes>
+
     </BrowserRouter>
+
   );
+
 }
 
-export default App
+export default App;
